8ccdf29e51ffb28cb3c127690d41e23a7af4bee1
Danielle <danielle@daniellemullane.com>
2015-04-13 11:34:46 -0600

    Complete exercise 6

--- exercises/06_any.rb
+++ exercises/06_any.rb
@@ -1,4 +1,14 @@
-
+def any?(items, &block)
+  result = false
+  if items == []
+    return result
+  else
+    items.each do |item|
+      result = true if block.call(item) || result
+    end
+    return result
+  end
+end
 
 # ------ code above this line ------
 
